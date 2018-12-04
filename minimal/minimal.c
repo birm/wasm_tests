2c11bc09a835cf8d973de149c039f9e11504f136
birm <birm@rbirm.us>
2018-12-03 23:04:48 -0500

    interactive

--- minimal/minimal.c
+++ minimal/minimal.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
+#include <sys/time.h>
+#include <emscripten/emscripten.h>
 
-int main(int argc, char ** argv) {
-  printf("Hello World\n");
+int EMSCRIPTEN_KEEPALIVE getTimeNow (int argc, char **argv)
+{
+  int iRet;
+  struct timeval tv;
+
+  iRet = gettimeofday (&tv, NULL); // timezone structure is obsolete
+  if (iRet == 0)
+  {
+    printf ("Seconds/USeconds since epoch: %d/%d\n",
+            (int)tv.tv_sec, (int)tv.tv_usec);
+    return (int)tv.tv_sec;
+  }
+  else
+  {
+    perror ("gettimeofday");
+  }
+
+  return iRet;
 }

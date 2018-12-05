a5dd132637f28325f4d1e5d7bb018d79de6f1b1d
birm <birm@rbirm.us>
2018-12-05 16:24:47 -0500

    array passing

--- openslide/slidetest.c
+++ openslide/slidetest.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-#include <openslide.h>
+#include <stdint.h>
 #include <emscripten/emscripten.h>
 
-int EMSCRIPTEN_KEEPALIVE getMpp (uint8_t **filedata)
+uint8_t EMSCRIPTEN_KEEPALIVE getMpp (uint8_t *filedata)
 {
   // convert file data to something openslide can understand
   // open it
   // get mpp-x
-  return 0
+  return filedata;
 }

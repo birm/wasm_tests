7eebd98b55e35e491a10af075b258f3d92f95bcb
birm <birm@rbirm.us>
2018-12-05 14:51:57 -0500

    start openslide mpp test

---
+++ openslide/slidetest.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <openslide.h>
+#include <emscripten/emscripten.h>
+
+int EMSCRIPTEN_KEEPALIVE getMpp (uint8_t **filedata)
+{
+  // convert file data to something openslide can understand
+  // open it
+  // get mpp-x
+  return 0
+}
